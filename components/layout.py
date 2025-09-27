9e958e36535ec3fe5c02d623082c5b0bd8e03d67
codeboyz12 <chanutsunatho@gmail.com>
2025-09-28 00:53:55 +0700

    install dash with simple project structure

---
+++ components/layout.py
@@ -0,0 +1,11 @@
+from dash import html
+import pandas as pd
+from components.widget import generate_table
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+def layout():
+    return html.Div([
+        html.H4(children='US Agriculture Exports (2011)'),
+        generate_table(df)
+    ])
---
+++ components/widget.py
@@ -0,0 +1,16 @@
+from dash import Dash, html
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ], style={
+        'border': '1px solid black',
+        'borderCollapse': 'collapse'
+    })
